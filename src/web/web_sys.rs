726e5f6d8e56f0c33694da8656dedd222d51a01c
Ryan Goldstein <ryan@ryanisaacg.com>
2019-12-22 13:57:58 -0500

    Fix web_sys imports of futures_util

--- src/web/web_sys.rs
+++ src/web/web_sys.rs
@@ -1,6 +1,5 @@
 use super::{new_wasm_error, web_try};
-use futures_util::future::{poll_fn, ready};
-use futures_util::try_future::TryFutureExt;
+use futures_util::future::{poll_fn, ready, TryFutureExt};
 use js_sys::Uint8Array;
 use std::{
     future::Future,
