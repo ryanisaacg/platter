514e5a52847c708fc0f217e68333749c15736a35
Ryan G <ryanisaacg@users.noreply.github.com>
2019-12-22 10:44:32 -0500

    Update futures dependency to futures 0.3 (#3)
    This switches from the preview crate to the released version

--- src/web/stdweb.rs
+++ src/web/stdweb.rs
@@ -1,6 +1,5 @@
 use super::{new_wasm_error, web_try};
-use futures_util::future::{poll_fn, ready};
-use futures_util::try_future::TryFutureExt;
+use futures_util::future::{poll_fn, ready, TryFutureExt};
 use std::{
     future::Future,
     io::Error as IOError,
